b26f070b77cb3e9e20dbcb62abc2affcd327c036
Luca Baradel <luca@Lucas-MacBook-Pro.local>
2017-10-11 13:24:38 -0700

    Sligthly better script

--- tcp_read.js
+++ tcp_read.js
@@ -1,6 +1,5 @@
-var r=require('/Users/luca/repo/libsbp/javascript/sbp/sbp.js');
 var net = require('net');
-var crc16buf = require('/Users/luca/repo/libsbp/javascript/sbp/msg').crc16;
+var msg = require('/Users/luca/repo/libsbp/javascript/sbp/msg');
 var client = new net.Socket();
 var HOST='192.168.0.222';
 var PORT='55555';
@@ -9,8 +8,10 @@ var validate_crc = true;
 const delta_report = 0;
 const SYNCWORD = '55';
 
+const MAX_DT = 100;
+
 var num_frames = 0;
-var MAX_FRAMES = 3;
+var MAX_FRAMES = -1;
 
 var myData = Buffer.alloc(0);
 
@@ -20,7 +21,9 @@ client.connect(PORT, HOST, function() {
 
 client.setTimeout(5000, function() {  client.destroy(); });
 
-var offset = 0;
+
+var last_tow = 0;
+var last_time = 0;
 
 client.on('data', function(data) {
   myData = Buffer.concat([myData, data]);
@@ -34,7 +37,6 @@ client.on('data', function(data) {
 
     var msg_type = myData.readUInt16LE(idx+1);
     var len = myData.readUInt8(idx+5);
-    console.log("\n\nmsg_type " + msg_type.toString(16) + " len " + len);
 
     /* Check we can go all the way to the CRC */
     if (myData.length < idx+8+len) {
@@ -43,28 +45,34 @@ client.on('data', function(data) {
 
     /* Validate CRC */
     var crc16 = myData.readUInt16LE(idx+6+len);
-    var mycrc = crc16buf(myData.slice(idx+1,idx+6+len));
+    var mycrc = msg.crc16(myData.slice(idx+1,idx+6+len));
     if (validate_crc && (crc16 != mycrc)) {
-      console.err("CRC does not match! Calculated " + mycrc.toString(16)  + " read " + crc16.toString(16));
+      console.log("CRC does not match! Calculated " + mycrc.toString(16)  + " read " + crc16.toString(16));
+      console.log("len " + len);
       myData = myData.slice(idx+len);
       continue;
     }
 
-    if (msg_type == 74) {
-      var tow = myData.readUInt32LE(idx+6);
+    var sbpmsg = msg.decode(myData.slice(idx));
+    //console.log(sbpmsg);
+
+    if (sbpmsg.sbp.msg_type == 0x4a) {
+      //console.log(sbpmsg.messageType);
+      tow = sbpmsg.fields.header.t['tow'];
       var now = Date.now();
-      delta = now - tow;
-      if (offset == 0) {
-        offset = delta;
-      }
-      delta -= offset;
-      delta = Math.abs(delta);
-      if (delta > delta_report) {
-        console.log("TOW is " + tow + " now is " + now + " delta is " + delta);
+      if (last_tow != 0) {
+        delta = tow - last_tow;
+        delta_host_time = now - last_time;
+        if (delta > MAX_DT || delta_host_time > MAX_DT) {
+          console.log("\tERROR delta " + delta + " host_time_delta " + delta_host_time);
+          console.log("\ttow " + tow + " now " + now);
         }
+      }
+      last_tow = tow;
+      last_time = now;
     }
 
-    myData = myData.slice(idx+len);
+    myData = myData.slice(idx+len+8);
 
     num_frames++;
     if (num_frames == MAX_FRAMES) {
